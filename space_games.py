d3b31996d83bad7e86e2104fe3fbef6727f73d41
IsmarIsmazov <ismar110501@gmail.com>
2023-05-02 00:51:18 +0600

    started

---
+++ space_games.py
@@ -0,0 +1,24 @@
+import pygame, controls
+from gun import Gun
+from pygame.sprite import Group
+
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((700, 800))
+    pygame.display.set_caption("Космические защитники")
+    bg_color = (0, 0, 0)
+    gun = Gun(screen)
+    bullets = Group()
+    inos = Group()
+    controls.create_army(screen, inos)
+    while True:
+        controls.events(screen, gun, bullets)
+        gun.update_gun()
+        bullets.update()
+        controls.update(bg_color, screen, gun, inos, bullets)
+        controls.update_bullet(bullets)
+        controls.update_inos(inos)
+
+
+run()
